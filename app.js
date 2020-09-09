69adc47e0a70e54881fb6c6ac231ccc610194d5d
Voya96 <shavoya.beecher@gmail.com>
2020-09-08 20:15:45 -0400

    Year 2 - I added more elements. My power grows with my dedication and hardwork

--- app.js
+++ app.js
@@ -10,5 +10,30 @@ console.log(h1)
 h1.innerText = "Hogwarts"
 container.appendChild(h1)
 
+const h2 = document.createElement("h2")
+console.log(h2)
+h2.innerText = "Shavoya"
+container.appendChild(h2)
+
+const h3 = document.createElement("h3")
+console.log(h3)
+h3.innerText = "Slytherin"
+container.appendChild(h3)
+
+const pet = document.createElement("h4")
+console.log(pet)
+pet.innerText = "Amaya"
+container.appendChild(pet)
+pet.className = "cat";
+
+const wand = document.createElement("h4")
+console.log(wand)
+wand.innerText = "Vine wood with a unicorn hair core, 13 ¾ and slightly yielding flexibility"
+container.appendChild(wand)
+
+
+
+
+
 
 
